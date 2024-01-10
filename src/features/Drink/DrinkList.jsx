41020237ed1f54083fdf06c843fdd8b6427fb9ce
mejat9806 <110531287+mejat9806@users.noreply.github.com>
2024-01-10 16:59:24 +0800

    drink font change

--- src/features/Drink/DrinkList.jsx
+++ src/features/Drink/DrinkList.jsx
@@ -1,11 +1,13 @@
+import { formatCurrency } from "../../utils/helper";
+
 function DrinkList({ drink: { name, price } }) {
   console.log(name, price);
 
   return (
     <div className=" mx-5 ">
-      <p className="flex justify-between text-4xl capitalize font-menuTitle text-left">
-        <span>{name}</span>
-        <span>{price}</span>
+      <p className="flex justify-between text-xl capitalize  text-left">
+        <span className="font-menuTitle">{name}</span>
+        <span>{formatCurrency(price)}</span>
       </p>
     </div>
   );
