e13d962557fbbad011f152bd1a9b1c33744f049c
mejat9806 <mejathussin@gmail.com>
2024-02-10 02:22:13 +0800

    Revert "Update Home.jsx"
    This reverts commit acf77146bd3e478b7cb737de39a5da368a53ac90.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,7 +68,7 @@ function Home() {
         <MenuHome />
       </div>
       <div className="flex justify-center bg-black items-center align-middle pb-2 ">
-        <LogoForSection> TRY OUR DRINKddede</LogoForSection>
+        <LogoForSection> TRY OUR DRINK</LogoForSection>
       </div>
       <div
         className={`${
